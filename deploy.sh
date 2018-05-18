7d91b18cd5b2b29375318d3aa1fe94a4a492d9db
Noah D. Brenowitz <nbren12@gmail.com>
2018-05-17 20:38:11 -0700

    Add GRL neural network paper

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ hugo
 find public/ -type d -exec chmod 0755 {} +
 find public/ -type f -exec chmod 0644 {} +
 
-if [ -d $HOME/public_html/ ]
+if [ -d  $HOME/tmp/webapp/public_html/ ]
 then
-    rsync  -avz --delete public/ ~/public_html/personal
+    rsync  -avz --delete public/ nbren12@noahbrenowitz.com:~/webapp/public_html/personal
 else
     rsync  -avz --delete public/ olympus:~/public_html/personal
 fi
